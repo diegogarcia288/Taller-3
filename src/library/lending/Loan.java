d8da1cc9dd3bfc2ee25002ee81fbb47ec9dc8a17
cnway.git <cway5338@gmail.com>
2026-09-15 17:32:30 -0500

    Add files via upload

---
+++ src/library/lending/Loan.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package library.lending;
+
+/**
+ *
+ * @author User
+ */
+public class Loan {
+    
+    private loanedOn LocalDate;
+    private dueOn localDate;
+    private returned boolean;
+    
+}
